9006356cd9851db081d4fb39a307208f8c9cee2f
Saravenpi Shigeturi <saravenpi@tuta.io>
2022-06-21 18:32:53 +0200

    fixed kettle repo path

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 mod action_handler;
+extern crate dirs;
+
 use action_handler::handle_action;
 
 fn main() {
     let mut args = std::env::args();
 
-    const KETTLE_PATH: &str = "~/.kettle/";
-    handle_action(&mut args, KETTLE_PATH);
+
+    match dirs::home_dir() {
+        Some(path) => {
+            let home_dir: &str = &path.display().to_string();
+            let kettle_path = vec![home_dir, "/.kettle/"].concat();
+            handle_action(&mut args, &kettle_path);
+
+        },
+        None => println!("Impossible to get your home dir!"),
+    }
 }
