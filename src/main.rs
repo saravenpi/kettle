b90e09a7b7c3e89a0dc3446745a072b4685dbdc6
Saravenpi Shigeturi <saravenpi@tuta.io>
2022-06-25 13:15:19 +0200

    create kettle repo path if not existing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 mod action_handler;
+use std::path::Path;
+use std::fs;
 extern crate dirs;
 
 use action_handler::handle_action;
@@ -10,6 +12,9 @@ fn main() {
         Some(path) => {
             let home_dir: &str = &path.display().to_string();
             let kettle_path = vec![home_dir, "/.config/kettle/"].concat();
+            if !Path::new(&kettle_path).exists() {
+                fs::create_dir_all(&kettle_path).expect("Error creating directory structure");
+            }
             handle_action(&mut args, &kettle_path);
         }
         None => println!("Impossible to get your home dir!"),
